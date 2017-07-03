ca7bac216a44767de151ec46fdd20d7f1ea4b867
dkbmevi <anerweiss@gmail.com>
2017-07-03 17:45:51 +0300

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,23 @@
+#### Plot1.R
+cat("\f")   
+library(dplyr)
+library(base)
+library(ggplot2)
+library(lattice)
+
+setwd("C:\\Users\\Aner\\Desktop\\C4_W1")
+HPC <-read.table("household_power_consumption.txt",header = TRUE,sep=";")  ###   2,075,259[rows]X9[cols]
+# Date;Time;Global_active_power;Global_reactive_power;Voltage;Global_intensity;Sub_metering_1;Sub_metering_2;Sub_metering_3?
+
+#Read only dates 2007-02-01 and 2007-02-02
+HPC_sub <- subset(HPC, Date == "1/2/2007" | Date == "2/2/2007") ### 2880[rows]X9[cols]                  
+#may be useful to convert the Date and Time variables to Date/Time classes in R using the strptime()  and as.Date() functions.
+#in this dataset missing values are coded as ?.
+
+x<- HPC_sub$Global_active_power
+x<-as.numeric(levels(x))[x]
+hist(x,col="red", main="Global Active Power",xlab="Global Active Power (kilowatts)",ylab="Frequency")
+dev.copy(png,'plot1.png')
+dev.off()
+
+
